190023ca9cb4b4489f60daf3fbe4e03ebc94320d
mohamed ayman <mohammed.ayman152433@gmail.com>
2025-10-20 16:22:50 +0300

    hero

---
+++ app/components/Hero.jsx
@@ -0,0 +1,55 @@
+"use client"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
+const Hero = () => {
+    useGSAP(() => {
+        const heroSplit = new SplitText(".title", {type:"chars words"});
+        const paraSplit = new SplitText(".subtitle", {type:"lines"});
+
+        heroSplit.chars.forEach((char) => {char.classList.add("text-gradient")});
+        gsap.from(heroSplit.chars, {
+            yPercent:100,
+            duration:1.8,
+            ease:"expo.out",
+            stagger:0.09
+        });
+        gsap.from(paraSplit.lines,{
+            opacity:0,
+            yPercent:100,
+            duration:0.9,
+            ease:"expo.out",
+            stagger:0.06,
+            delay:1
+        })
+    },[])
+    return(
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">MOJITO</h1>
+
+                {/* leaf */}
+                <img src="/images/hero-left-leaf.png" alt="leaf" className="left-leaf"/>
+                <img src="/images/hero-right-leaf.png" alt="leaf" className="right-leaf"/>
+
+                {/* body */}
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Calssic</p>
+                            <p className="subtitle">
+                                Sib the Sprit <br /> of Summer 
+                            </p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes — designed to delight your senses. </p>
+                            <a href="#cocktails">view cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default Hero;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { gsap } from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
 const App = () => {
   return(
-    <div className={"flex-center h-screen noisy"}>
-      <div className={"text-gradient"}>t</div>
-    </div>
+    <main className="main">
+      <Navbar/>
+      <Hero/>
+    </main>
   )
 }
 export default App;
